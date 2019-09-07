726fb0469e8fbf9f70f8d0537a2fb4a8b29556c5
JToton <37555826+JToton@users.noreply.github.com>
2019-09-07 07:04:09 -0600

    Prototype Start
    Added current state of project.
    -Movement
    -Simple Navmesh AI and Enemy
    -Basic hp, shooting, and damage yet to be completed.

---
+++ CC - Proto/Assets/Scifi/Scripts/DoorFunctions.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorFunctions : MonoBehaviour {
+
+    static Animator Door_Anim;
+    
+    public GameObject Locker;
+    public GameObject Sound;
+
+
+    private void Awake()
+    {
+    Door_Anim = GetComponent<Animator>();
+
+    }
+  
+
+   void OnTriggerEnter(Collider col)   
+    {
+
+        if(col.gameObject.tag == "Player")
+        {
+
+            Door_Anim.SetBool("Door_Open",true);
+            Locker.SetActive(true);
+            StartCoroutine(Timmer());
+            // plays sound when door opens.
+            // Sound.SetActive(true); 
+
+        }
+       
+
+    }
+
+    IEnumerator Timmer()
+    {
+       
+        yield return new WaitForSeconds(4);
+        Door_Anim.SetBool("Door_Open", false);
+        Locker.SetActive(false);
+        // plays sound when door closes
+        // Sound.SetActive(false);
+
+
+    }
+
+
+}
+	
+	
