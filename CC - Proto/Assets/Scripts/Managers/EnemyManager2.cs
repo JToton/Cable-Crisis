7f45e5066d6dae5749c1336c5332e303c63abf03
JToton <37555826+JToton@users.noreply.github.com>
2019-09-19 13:41:17 -0600

    Made the Demo hard
    Enemies start to spawn on both sides
    increased shot amounts
    left damage the same

---
+++ CC - Proto/Assets/Scripts/Managers/EnemyManager2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager2 : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public GameObject enemy;
+    EnemyHealth enemyHealth;          //reference the other script
+
+
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+    public int totalEnemyAmount = 10;
+    public int enemyAmount = 0;
+
+    private void Awake()
+    {
+        enemyHealth = GetComponentInChildren<EnemyHealth>();
+    }
+
+    void Start()
+    {
+        Invoke("spawnEnemiesTwo",20f);
+    }
+
+    void spawnEnemiesTwo()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    private void Update()
+    {
+
+        if (enemyAmount == totalEnemyAmount)
+        {
+            CancelInvoke();
+        }
+        /*
+        if (enemyHealth.isDead)
+        {
+            enemyAmount -= 1;
+        }
+        */
+
+    }
+
+    void Spawn()
+    {
+        if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        enemyAmount += 1;
+    }
+}
